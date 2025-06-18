196dda9d8a4f8f6ff9633e24ba98ff768b573561
Noah Stephen <n04hstephen@gmail.com>
2025-06-18 21:14:18 +0700

    new google app sript???? niggga

--- backend/api/upload.js
+++ backend/api/upload.js
@@ -7,8 +7,8 @@ export default async function handler(req, res) {
     const { fullName, email, phone, message, anonymous, imageBase64 } = req.body;
 
     // Forward to Google Apps Script
-    const scriptUrl = 'https://script.google.com/macros/s/AKfycbzPN9SFQr-Tm4dJPk5CtOOzLLU7urPZvoiSiqgCQZpSLDUG_9Yr9carGUBpj1TSBVq5/exec';
-
+    const scriptUrl = 'https://script.google.com/macros/s/AKfycbzT5eSqPA-0Obfr58bGNMQvb8J1kdNg8xmkSeDQq7Y7qO56OsIyCnZfMt7HW1RHb0r8/exec';
+                      
     const forward = await fetch(scriptUrl, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
